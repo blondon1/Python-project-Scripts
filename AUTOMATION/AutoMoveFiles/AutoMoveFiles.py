6ec435a9977f052fa67278e542b1484f88ef4cbf
Bryan <91551702+blondon1@users.noreply.github.com>
2024-01-24 10:02:03 -0500

    Update AutoMoveFiles.py
    The on_modified event is triggered for any modification, it is more efficient to specifically check if the modification refers to a new file being added

--- AUTOMATION/AutoMoveFiles/AutoMoveFiles.py
+++ AUTOMATION/AutoMoveFiles/AutoMoveFiles.py
@@ -8,11 +8,30 @@ import os
 import json
 
 class Handler(FileSystemEventHandler):
+    def __init__(self, watched_folder, destination_folder):
+        self.watched_folder = watched_folder
+        self.destination_folder = destination_folder
+
     def on_modified(self, event):
-        for file in os.listdir(watched_folder):
-            src = f"{watched_folder}/{file}"
-            dst = f"{destination_folder}/{file}"
-            os.rename(src=src, dst=dst)
+        if not event.is_directory:
+            # List only files to avoid moving directories
+            files = [f for f in os.listdir(self.watched_folder) if os.path.isfile(os.path.join(self.watched_folder, f))]
+            for file in files:
+                src = os.path.join(self.watched_folder, file)
+                dst = os.path.join(self.destination_folder, file)
+                if not os.path.exists(dst):
+                    os.rename(src, dst)
+                    logging.info(f"Moved: {src} -> {dst}")
+                else:
+                    # Handle duplicate files by appending an index
+                    base, extension = os.path.splitext(dst)
+                    counter = 1
+                    new_dst = f"{base}_{counter}{extension}"
+                    while os.path.exists(new_dst):
+                        counter += 1
+                        new_dst = f"{base}_{counter}{extension}"
+                    os.rename(src, new_dst)
+                    logging.info(f"Moved: {src} -> {new_dst}")
 
 if __name__=="__main__":
     watched_folder = input("Paste the path to the folder to be tracked: ")
